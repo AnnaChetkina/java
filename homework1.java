47fbc88f8376082ab2f432965927e2014418c12d
Anna <a.chetkina.a@yandex.ru>
2023-01-26 21:23:31 +0300

    add fn getTriangularNumber

--- homework1.java
+++ homework1.java
@@ -1,2 +1,15 @@
+import java.util.List;
+
+// Написать программу вычисления n-ого треугольного числа
 public class homework1 {
+    public static void main(String[] args) {
+        getTriangularNumber(5); // 15
+        getTriangularNumber(7); // 28
+        getTriangularNumber(10); // 55
+    }
+
+    static void getTriangularNumber(int num) {
+        int res = (num * (num + 1)) / 2;
+        System.out.println(res);
+    }
 }
