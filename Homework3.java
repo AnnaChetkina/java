a9c41c5a25b787a8148b0438c576e9da43502ba1
Anna <a.chetkina.a@yandex.ru>
2023-02-16 18:38:14 +0300

    add Homework3 ver.2

--- Homework3.java
+++ Homework3.java
@@ -7,55 +7,92 @@
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Scanner;
 
 public class Homework3 {
     public static void main(String[] args) {
-//        System.out.println(getFactorial(6));
-        System.out.println(getRoutesCount(5, 5)); // 137846528820
+//        System.out.println(getRoutesCount(getInitCoordinates(), 10, 8));
+        System.out.println(getRoutesCount(getInitCoordinates(),8, 8));
+//        System.out.println(getRoutesCount(getInitCoordinates(), 0, 2));
 
-    }
+//        System.out.println(getRoutesCount(3, 3));
+//        System.out.println(getRoutesCount(8, 8));
+//        System.out.println(getRoutesCount(9, 8));
 
-    public static long getFactorial(int num) {
-        int result = 1;
-        for (int i = 1; i <= num; i++) {
-            result = result * i;
-        }
-        return result;
-    }
 
-    public static long getRoutesCount(int n, int m){
-        long res = 0;
-//        n -= 1;
-//        m -= 1;
-        System.out.println(n);
-        System.out.println(m);
-//
-//        long factSum = getFactorial(n * m);
-//        long factN = getFactorial(n);
-//        long factM = getFactorial(m);
-        System.out.println(getFactorial(n * m));
-//        System.out.println(factN);
-//        System.out.println(factM);
-//        long factMult = factN * factM;
-        long r = getFactorial(n) * getFactorial(m);
-//        System.out.println(factMult);
-//        res = factSum / factMult;
-        res = getFactorial(n * m) / (getFactorial(n) * getFactorial(m));
-        return res;
-
-//        M -= 1
-//        N -= 1
-//        res = factorial(N+M)//(factorial(N)*factorial(M))
     }
 
     static Map<String, Integer> getInitCoordinates() {
 
         Map<String, Integer> initCoordinates = new HashMap<>();
+        int x = 0;
+        int y = 0;
 
-//        initCoordinates.put("a", a);
-//        initCoordinates.put("b", b);
+        Scanner in = new Scanner(System.in);
+        System.out.print("Input initial coordinate x: ");
+        x = in.nextInt();
+        System.out.print("Input initial coordinate y: ");
+        y = in.nextInt();
+        in.close();
+
+        initCoordinates.put("x", x);
+        initCoordinates.put("y", y);
 
         return initCoordinates;
+    }
+
+    public static int getRoutesCount(Map<String, Integer> initCoordinates, int n, int m){
+        if ((n < 1 || m < 1) || (n == 1 && m == 1)){
+            System.out.println("err 1");
+            return 0;
+        }
+
+        Integer x = initCoordinates.get("x");
+        Integer y = initCoordinates.get("y");
+
+        System.out.println("============");
+        System.out.println(x);
+        System.out.println(y);
+        System.out.println("============");
+
+
+
+
+        if (x >= n || y >= m){
+            System.out.println("err");
+            return 0;
+        }
+
+        int[][] multi = prefillArr(n, m);
+
+//        for (int i = 0; i < n; i++) {
+//            for (int j = 0; j < m; j++) {
+//                System.out.println(multi[i][j]);
+//            }
+//        }
+
+        for (int i = 1; i < n; i++) {
+            for (int j = 1; j < m; j++) {
+                multi[i][j] = multi[i-1][j] + multi[i][j-1];
+//                System.out.println(multi[i][j]);
+//                System.out.println("+++++++++++++");
+            }
+        }
+        System.out.println(multi[n - 1][m - 1]);
+
+        return multi[n - 1][m - 1];
+    }
+
+
+    public static int[][] prefillArr(int n, int m){
+        int[][] multi = new int[n][m];
+        for (int i = 1; i < multi[0].length; i++) {
+            multi[i][0] = 1;
+        }
+        for (int i = 1; i < multi[1].length; i++) {
+            multi[0][i] = 1;
+        }
+        return multi;
     }
 }
 
