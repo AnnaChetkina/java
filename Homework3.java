e59403d03e584b430098884cacd0fd311086eb9b
Anna <a.chetkina.a@yandex.ru>
2023-02-16 19:10:37 +0300

    add Homework3 final ver

--- Homework3.java
+++ Homework3.java
@@ -13,16 +13,11 @@ public class Homework3 {
     public static void main(String[] args) {
 //        System.out.println(getRoutesCount(getInitCoordinates(), 10, 8));
         System.out.println(getRoutesCount(getInitCoordinates(),8, 8));
-//        System.out.println(getRoutesCount(getInitCoordinates(), 0, 2));
-
-//        System.out.println(getRoutesCount(3, 3));
-//        System.out.println(getRoutesCount(8, 8));
-//        System.out.println(getRoutesCount(9, 8));
-
+//        System.out.println(getRoutesCount(getInitCoordinates(), 0, 2)); // проверка некорректного ввода
 
     }
 
-    static Map<String, Integer> getInitCoordinates() {
+    static Map<String,   Integer> getInitCoordinates() {
 
         Map<String, Integer> initCoordinates = new HashMap<>();
         int x = 0;
@@ -43,54 +38,37 @@ public class Homework3 {
 
     public static int getRoutesCount(Map<String, Integer> initCoordinates, int n, int m){
         if ((n < 1 || m < 1) || (n == 1 && m == 1)){
-            System.out.println("err 1");
+            System.out.println("Введено некорректное значение n или m");
             return 0;
         }
 
         Integer x = initCoordinates.get("x");
         Integer y = initCoordinates.get("y");
 
-        System.out.println("============");
-        System.out.println(x);
-        System.out.println(y);
-        System.out.println("============");
-
-
-
-
-        if (x >= n || y >= m){
-            System.out.println("err");
+        if ((x >= n || y >= m) || (x <= 0 || y <= 0)){
+            System.out.println("Введено некорректное значение точки(Х, Y)");
             return 0;
         }
 
         int[][] multi = prefillArr(n, m);
 
-//        for (int i = 0; i < n; i++) {
-//            for (int j = 0; j < m; j++) {
-//                System.out.println(multi[i][j]);
-//            }
-//        }
+        x -= 1;
+        y -= 1;
 
-        for (int i = 1; i < n; i++) {
-            for (int j = 1; j < m; j++) {
+        for (int i = 1; i < n - x; i++) {
+            for (int j = 1; j < m - y; j++) {
                 multi[i][j] = multi[i-1][j] + multi[i][j-1];
-//                System.out.println(multi[i][j]);
-//                System.out.println("+++++++++++++");
             }
         }
-        System.out.println(multi[n - 1][m - 1]);
-
-        return multi[n - 1][m - 1];
+        return multi[n - x - 1][m - y - 1];
     }
-
 
     public static int[][] prefillArr(int n, int m){
         int[][] multi = new int[n][m];
-        for (int i = 1; i < multi[0].length; i++) {
-            multi[i][0] = 1;
-        }
-        for (int i = 1; i < multi[1].length; i++) {
-            multi[0][i] = 1;
+        for (int i = 0; i < multi[0].length; i++) {
+            for (int j = 0; j < multi[1].length; j++) {
+                multi[i][j] = 1;
+            }
         }
         return multi;
     }
